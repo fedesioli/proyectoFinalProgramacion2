ce985dba5ecf0b49599c65551fe026b0b56c7489
Federico Sioli <fsioli@udesa.edu.ar>
2020-05-12 20:44:44 -0300

    Intento de que funcione el css y js de home

--- proyectoIntegradorProgra2/routes/rutasPrincipales.js
+++ proyectoIntegradorProgra2/routes/rutasPrincipales.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var controlador = require("../controllers/home")
 
 router.get("/", controlador.home);
-router.get("/generos", controlador.home);
+router.get("/buscadorAvanzado", controlador.home);
 router.get("/favoritos", controlador.home);
-router.get("/", controlador.home)
+router.get("/detalle/:id", controlador.home)
 
 module.exports = router;
